61a0545c0b2e38a5038c963a105f0d9ed71a0284
sbor0018 <sbor0018@student.monash.edu>
2022-10-30 11:40:38 +0800

    Docs done

--- food.py
+++ food.py
@@ -1,8 +1,11 @@
+""" Food class that allows us to work with food objects in the game"""
 from __future__ import annotations
 from random import random
 
 from material import Material
 from random_gen import RandomGen
+
+__author__ = 'Jun Yu Tan'
 
 # List of food names from https://github.com/vectorwing/FarmersDelight/tree/1.18.2/src/main/resources/assets/farmersdelight/textures/item
 FOOD_NAMES = [
@@ -100,18 +103,26 @@ class Food:
         1) name of food
         2) hunger bars food provides
         3) price of the food
+        :complexity: Best and worst case complexity is O(1)
         '''
         self.name = name 
         self.hunger_bars = hunger_bars 
         self.price = price
     
     def __str__(self) -> str:
+        '''
+        Generate string representation of the food item
+        :returns: string representation of food
+        :complexity: Best and worst case complexity is O(1)
+        '''
         return self.name + " gives " + str(self.hunger_bars) + " hunger bars and costs " + str(self.price)
 
     @classmethod
     def random_food(cls) -> Food:
         '''
         Generate random instance of Food
+        :returns: the food object that is randomly created
+        :complexity: Best and worst case complexity is O(1)
         '''
         random_name = RandomGen.random_choice(FOOD_NAMES)
         random_hunger_bars = RandomGen.randint(50,500)
